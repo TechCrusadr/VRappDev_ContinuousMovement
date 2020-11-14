44731a0bdccce009da70460b4d4492c74886b867
Justin Chow <jjc2184@caa.columbia.edu>
2020-11-14 12:36:55 -0800

    added ability to log joystick and trigger button vals

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -33,5 +33,13 @@ public class HandPresence : MonoBehaviour
         targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
         if (primaryButtonValue)
             Debug.Log("Pressing Primary Button");
+
+        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
+        if (triggerValue > 0.1f)
+            Debug.Log("Trigger pressed " + triggerValue);
+
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        if (primary2DAxisValue != Vector2.zero)
+            Debug.Log("Primary Touchpad " + primary2DAxisValue);
     }
 }
