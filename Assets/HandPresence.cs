ac02827c6c46bed4008ffd7448f56bb6c1b42aff
Justin Chow <jjc2184@caa.columbia.edu>
2020-11-14 12:56:08 -0800

    added Quest1 and Quest2 controller prefabs from Oculus

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -5,7 +5,9 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public List<GameObject> controllerPrefabs;
     private InputDevice targetDevice;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,16 +32,13 @@ public class HandPresence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
-        if (primaryButtonValue)
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
             Debug.Log("Pressing Primary Button");
 
-        targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue);
-        if (triggerValue > 0.1f)
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
             Debug.Log("Trigger pressed " + triggerValue);
-
-        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
-        if (primary2DAxisValue != Vector2.zero)
+          
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
             Debug.Log("Primary Touchpad " + primary2DAxisValue);
     }
 }
