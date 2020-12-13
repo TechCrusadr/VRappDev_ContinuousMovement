e5de9e0f6ad082c35404de29a1fb8ff0a2876126
Justin Chow <jjc2184@caa.columbia.edu>
2020-12-12 23:54:38 -0800

    added snap turn; added capsule follow headset

--- Assets/ContinousMovement.cs
+++ Assets/ContinousMovement.cs
@@ -10,7 +10,7 @@ public class ContinousMovement : MonoBehaviour
     public XRNode inputSource;
     public float gravity = -9.81f;
     public LayerMask groundLayer;
-
+    public float additionalHeight = 0.2f;
     private float fallingSpeed;
     private XRRig rig;
     private Vector2 inputAxis;
@@ -33,6 +33,7 @@ public class ContinousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CapsuleFollowHeadset();
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
         character.Move(direction * Time.fixedDeltaTime * speed);
@@ -44,6 +45,12 @@ public class ContinousMovement : MonoBehaviour
         else
         fallingSpeed += gravity *Time.fixedDeltaTime;
         character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
     }
 
     bool CheckIfGrounded()
