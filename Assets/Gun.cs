ec758317ab9eb1d00b0ae63ce22fe235e71f2be4
Justin Chow <jjc2184@caa.columbia.edu>
2021-01-10 00:05:23 -0800

    added gun script, bullet prefab, and 9mm audio

---
+++ Assets/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float speed = 40;
+    public GameObject bullet;
+    public Transform barrel;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
+
+    // Start is called before the first frame update
+    
+    public void Fire()
+    {
+        GameObject spanwedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
+        spanwedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+        audioSource.PlayOneShot(audioClip);
+        Destroy(spanwedBullet, 2);
+    }
+}
